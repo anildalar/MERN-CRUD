f609c01463ad00a4c58be1f3c31b94ae83ac27b8
anildalar <dollorinfotech@gmail.com>
2021-09-04 08:46:28 +0530

    Fileuploading and Validation

--- simplecrud-backend/src/routes/deletePost.js
+++ simplecrud-backend/src/routes/deletePost.js
@@ -1,28 +1,25 @@
 const fs = require('fs'); // File System
+const path = require('path');
 //fs is a buit-in module of Nodejs
-
 const PostModel = require('../models/PostModel');
-
-
 let deletePost = (req,res)=>{
-       // let pid = req.params.postid;
-        //Model.findOneAndDelete(conditions, callback) // executes
+    // let pid = req.params.postid;
+    //Model.findOneAndDelete(conditions, callback) // executes
 
-        //1 . Delete the mongodb Document
-        PostModel.findOneAndDelete({ _id:req.params.postid },function(e,d){
-            if(e) res.status(400).json(e);
-
-            //2. Delete the file from server as well
-            
-            //console.log(d.image);
-            fs.unlinkSync('/uploads/'+d.image);
-            //deleteImage(d.image);
-            //console.log(d);
-            res.status(200).json({
-                msg:"Data Deleted Successfully",
-                data:d
-            });
-        })
+    //1 . Delete the mongodb Document
+    PostModel.findOneAndDelete({ _id:req.params.postid },function(e,d){
+        if(e) throw e;
+        //console.log(path.join("./uploads/" , d.image));
+        //2. Delete the file from server as well
+        //console.log(d.image);
+        //fs.unlinkSync(path.join("./uploads/" , d.image)); 
+        fs.unlinkSync("\\uploads\\" +d.image); 
+        //console.log('File deleted!');
+        res.status(200).json({
+            msg:"Data Deleted Successfully",
+            data:d
+        });           
+    })
 };
 
 module.exports = deletePost;
--- simplecrud-backend/src/routes/fileUpload.js
+++ simplecrud-backend/src/routes/fileUpload.js
@@ -2,27 +2,32 @@ const PostModel = require('../models/PostModel');
 let fileUpload = (req,res)=>{
     console.log(req.body.title);
     console.log(req.body.description);
-    console.log(req.file.filename);
-   // res.status(200).send({msg:"ok"});
-  
-    const postmodelObject = new PostModel({
-        title:req.body.title,
-        description:req.body.description,
-        image:req.file.filename
-    });
-    postmodelObject.save().then((d)=>{
-        res.status(201).json({
-            msg:"Data Stored Successfuly",
-            d:req.body,
-            msg:"File Uploaded Success"
-          });
-    }).catch((e)=>{
-        console.log(e);
-    }).finally((all)=>{
-
-    });
-
+    console.log();
+    if(req.file.filename.split('.')[1] == 'png' || req.file.filename.split('.')[1] == 'jpg' || req.file.filename.split('.')[1] == 'jpeg'){
+        const postmodelObject = new PostModel({
+            title:req.body.title,
+            description:req.body.description,
+            image:req.file.filename
+        });
+        postmodelObject.save().then((d)=>{ //d = response data from mongodb
+            res.status(201).json({
+                msg:"Data Stored Successfuly",
+                d:req.body,
+                resdata:d,
+                msg:"File Uploaded Success"
+              });
+        }).catch((e)=>{
+            console.log(e);
+        }).finally((all)=>{
     
+        });
+    }else{
+        res.status(400).send({
+            msg:"Please Upload image file only"
+        });
+    }
+
+   // res.status(200).send({msg:"ok"});
 }
 
 module.exports = fileUpload;
